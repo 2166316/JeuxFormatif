d3ab22420c08cbbc1837e78ccb277b3b1c1dd08e
Antoine Fortier <2166316@cegeplimoilou.ca>
2024-04-23 10:46:56 -0400

    ajout

---
+++ 2.01-MultiPlayer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+
+public class PlayerController : NetworkBehaviour
+{
+    [SerializeField] private float speed = 7f;
+    [SerializeField] private float rotationSpeed = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(!IsOwner) return;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDir = new Vector3(horizontalInput,0, verticalInput);
+        movementDir.Normalize();
+
+        transform.Translate(movementDir* Time.deltaTime * speed, Space.World);
+
+        if (movementDir != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDir, Vector3.up);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, toRotation,90);
+            transform.rotation = rotation;
+        }
+
+    }
+}
---
+++ 2.01-MultiPlayer/Assets/Scripts/TestCounter.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class TestCounter : NetworkBehaviour
+{
+    private NetworkVariable<int> counter = new NetworkVariable<int>(0,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+
+    void Start()
+    {
+        
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        Debug.Log(counter.Value + "");
+       // counter.OnValueChanged += OnCounterChanged;
+    }
+
+    private void OnCounterChanged(int? previous,int? current)
+    {
+        Debug.Log(counter.Value+"");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("Owner?" + IsOwner);
+            IncrementCounterServerRpc();
+            Debug.Log(counter.Value + "");
+        }
+    }
+
+    [Rpc(SendTo.Owner)]
+    public void IncrementCounterServerRpc()
+    {
+        counter.Value++;
+    }
+}
